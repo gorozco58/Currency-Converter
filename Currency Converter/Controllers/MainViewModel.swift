5b4498495ba8608c404d92c754c60bf2d68d7ac3
Giovanny Orozco <giovanny.orozco@rappi.com>
2017-01-03 10:53:24 -0500

    some changes

--- Currency Converter/Controllers/MainViewModel.swift
+++
@@ -1,9 +0,0 @@
-//
-//  MainViewModel.swift
-//  Currency Converter
-//
-//  Created by Giovanny Orozco on 1/3/17.
-//  Copyright © 2017 Giovanny Orozco. All rights reserved.
-//
-
-import Foundation
